d74d14a23da315ab3462b4d6a29a7e27504a16b4
turashbayevajr <103361172+turashbayevajr@users.noreply.github.com>
2023-03-30 16:39:35 +0600

    Exclude node_modules directory

---
+++ models/post.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const postSchema = new Schema({
+    image:{
+     type: String,
+    required: true,
+    
+    },
+    message:{
+        type: String,
+        required: true,
+    },
+    // user_id: Number,
+});
+Post=mongoose.model('Post', postSchema);
+module.exports = Post;
